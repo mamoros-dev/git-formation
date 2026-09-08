faff923df1e9b43252d8b832289c66e334762c5a
Miguel Amorós Moret <miguel14amoros@gmail.com>
2026-09-08 18:58:47 +0200

    feat(terraform): add configurable lambda memory variable

--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@ variable "lambda_timeout" {
   type        = number
   default     = 30
 }
+
+variable "lambda_memory" {
+  description = "Memory allocation in MB for the Lambda function"
+  type        = number
+  default     = 128
+}
