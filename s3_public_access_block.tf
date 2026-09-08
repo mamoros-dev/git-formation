f1700143d15519d4d7d2cf7ea1aa7ac0e74ddbd7
Miguel Amorós Moret <miguel14amoros@gmail.com>
2026-09-08 19:04:01 +0200

    Revert "hotfix(terraform): block public access on s3 bucket"
    This reverts commit f0d8f7d1ba574c312887582d5273444680a6d3e6.

--- s3_public_access_block.tf
+++
@@ -1,6 +0,0 @@
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket                  = aws_s3_bucket.example.id
-  block_public_acls       = true
-  block_public_policy     = true
-}
